ac409d0ebc4775a0236bb3d1ab44cbdda61bed69
harshitha <pottiharshitha24@gmail.com>
2024-02-19 16:07:22 +0530

    Student details

--- student_details.java
+++ student_details.java
@@ -20,9 +20,18 @@ public class student_details {
        // In the given code snippet, `Courses s = new Courses();` creates a new object of the `Courses`
        // class and assigns it to the variable `s`.
         Course s = new Course();
+       // The lines `s.Name = "Radha Harshitha";`, `s.Dept = "MCA";`, and `s.Prog = "BCA";` are
+       // assigning values to the instance variables `Name`, `Dept`, and `Prog` of the `Course` object
+       // `s`. This means that the student's name is set to "Radha Harshitha", the department is set to
+       // "MCA", and the program is set to "BCA". These values will be used when displaying the details
+       // of the student using the `displayDetails()` method.
         s.Name = "Radha Harshitha";
         s.Dept = "MCA";
         s.Prog = "BCA";
+        // `s.displayDetails();` is calling the `displayDetails()` method on the `Course` object `s`.
+        // This method is responsible for printing out the details of the student, including the
+        // student's name, department, and program. When `s.displayDetails();` is called, it will
+        // output this information to the console.
         s.displayDetails();
 }
 }
